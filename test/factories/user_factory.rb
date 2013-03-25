5d73318dbe4196e2f5ef41937b8ee43822ea248f
kirby00 <mbkirby@gmail.com>
2013-03-25 18:09:44 +0000

    Build basic user factory

---
+++ test/factories/user_factory.rb
@@ -0,0 +1,22 @@
+
+class UserFactory
+
+  def self.user
+    User.create!( name: "Example User", 
+                  email: "user@example.com",
+                  password: "foobar",
+                  password_confirmation: "foobar"
+                )
+  end
+
+  def self.user_without_password
+  end
+
+  def self.user_with_non_matching_password
+  end
+
+  def self.ten_users
+    Array.new(10) { user }
+  end
+  
+end
