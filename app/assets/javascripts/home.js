3ad8b64ae904f6baf30e65aa416cca4c788cf113
kirby00 <mbkirby@gmail.com>
2013-03-28 15:26:36 +0000

    Add placeholder for saving item to wishlist - using alert

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,30 +1,34 @@
 // Initiate Countdown
 
 jQuery(document).ready(function() {
+  if ($('#countdown_dashboard').length) {
     $('#countdown_dashboard').countDown({
-        targetOffset: {
-            'day':      1,
-            'month':    1,
-            'year':     2013,
-            'hour':     0,
-            'min':      0,
-            'sec':      0
-        }
+      targetOffset: {
+        'day':      1,
+        'month':    1,
+        'year':     2013,
+        'hour':     0,
+        'min':      0,
+        'sec':      0
+      }
     });
+  }
 });
 
 // Set by specific date/time
 function set_by_date() {
-    $('#countdown_dashboard').stopCountDown();
-    $('#countdown_dashboard').setCountDown({
-        targetDate: {
-            'day':      25,
-            'month':    12,
-            'year':     2013,
-            'hour':     12,
-            'min':      0,
-            'sec':      0
-        }
+  $('#countdown_dashboard').stopCountDown();
+  $('#countdown_dashboard').setCountDown({
+      targetDate: {
+        'day':      25,
+        'month':    12,
+        'year':     2013,
+        'hour':     12,
+        'min':      0,
+        'sec':      0
+      }
     });
-    $('#countdown_dashboard').startCountDown();
+  $('#countdown_dashboard').startCountDown();
 }
+
+
---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,8 @@
+$(function() {
+
+  $('.amazon_list .btn').click(function() {
+    alert('Sorry you cannot save this to your list currently, its a new feature coming soon');
+  });
+
+})
+
