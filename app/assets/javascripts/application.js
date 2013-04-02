3822dafa4a0cf8b73794d8ca2fe3670e481836cc
kirby00 <mbkirby@gmail.com>
2013-04-02 14:56:42 +0100

    JS user registration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,15 +16,58 @@
 //= require_tree .
 
 $(function(){
+  var name = $( "#name" ),
+  email = $( "#email" ),
+  password = $( "#password" ),
+  password_confirmation = $( "#password_confirmation" ),
+  allFields = $( [] ).add( name ).add( email ).add( password ).add( password_confirmation ),
+  tips = $( ".validateTips" );
+
+  function updateTips( t ) {
+    tips
+      .text( t )
+      .addClass( "ui-state-highlight" );
+    // setTimeout(function() {
+    //   tips.removeClass( "ui-state-highlight", 1500 );
+    // }, 500 );
+  }
+
+  function checkLength( o, n, min, max ) {
+    if ( o.val().length > max || o.val().length < min ) {
+      o.addClass( "ui-state-error" );
+      updateTips( "Length of " + n + " must be between " +
+        min + " and " + max + "." );
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   $( "#dialog-form" ).dialog({
     autoOpen: false,
-    height: 400,
+    height: 575,
     width: 350,
     modal: true,
+    buttons: {
+      "Register": function() {
+        var bValid = true;
+          allFields.removeClass( "ui-state-error" );
+
+          bValid = bValid && checkLength( name, "username", 1, 25 );
+          bValid = bValid && checkLength( email, "email", 6, 80 );
+          bValid = bValid && checkLength( password, "password", 6, 16 );
+          bValid = bValid && checkLength( password_confirmation, "password_confirmation", 6, 16 );
+
+          if ( bValid ) {
+            $.post('/users', { user: { name: name.val(), email: email.val(), password: password.val(), password_confirmation: password_confirmation.val() } });
+            $( this ).dialog( "close" );
+          }
+
+      }
+    }
   });
   $('.registration').click(function(e) {
     e.preventDefault();
     $( "#dialog-form" ).dialog("open");
-
   });
 });
