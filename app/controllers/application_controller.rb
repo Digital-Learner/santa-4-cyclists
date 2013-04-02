b8fa406bbf22d4de6e7c26ab6f48ef12e34bf55f
kirby00 <mbkirby@gmail.com>
2013-04-02 15:11:44 +0100

    Merge resolution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # Check with Max, don't think its required anymore!
-  #
-  # include Amazon::AWS
-  # include Amazon::AWS::Search
+  include SessionsHelper
 
   # # after_filter :check_with_amazon, :only => [:create, :update]
 
@@ -25,24 +22,5 @@ class ApplicationController < ActionController::Base
       # look at how to use cookies instead of session information for 'staying alive'
       # in Hartl example we are using remember_token and storing this in the user table (do we need?)
     end
-
-    # Check with Max, don't think its required anymore!
-    #
-    # def check_with_amazon
-    #   @check=@item.name
-    #   begin
-    #     is = ItemSearch.new( 'All', { 'Keywords' => @check } )
-    #     rg = ResponseGroup.new( 'Medium' )
-    #     req = Request.new
-    #     req.locale = 'us'
-    #     resp = req.search( is, rg )
-    #     items = resp.item_search_response.items.item
-    #   rescue
-    #     if Amazon::AWS::Error
-    #       @item.flag = 'false'
-    #       @item.save
-    #     end
-    #   end
-    # end
 
 end
