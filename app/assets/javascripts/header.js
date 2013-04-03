942749a0b3f34e5d816211702dc8acc49401d3ac
railyboy <s.m.github@gmail.com>
2013-04-03 13:09:24 +0100

    Fix validation error in js + refactor

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,58 +1,10 @@
-// $(function(){
-//     var initRegistrationDialog = function() {
-//       var name = $( "#name" ),
-//       email = $( "#email" ),
-//       password = $( "#password" ),
-//       password_confirmation = $( "#password_confirmation" ),
-//       allFields = $( [] ).add( name ).add( email ).add( password ).add( password_confirmation )
-//     },
-
-//     var initLoginDialog = function() {
-//       var email = $( "#email" );
-//       var password = $( "#password" );
-//       var allFields = $( [] ).add( email ).add( password );
-//       var loginHandler = function() {
-//         var bValid = true;
-//         allFields.removeClass( "ui-state-error" );
-
-//         bValid = bValid && checkLength( email, "email", 6, 80 );
-//         bValid = bValid && checkLength( password, "password", 6, 16 );
-
-//         if ( bValid ) {
-//           var params = { user: { email: email.val(), password: password.val() } };
-//           var callback = function() {
-//             window.location = '/';
-//           }
-//           $.post('/login', params, callback);
-//         }
-//       };
-//       $( "#login-form" ).dialog({
-//         autoOpen: false,
-//         height: 375,
-//         width: 350,
-//         modal: true,
-//         buttons: {
-//           Login: loginHandler
-//         }
-//       });
-//     },
-//   var bindNotLoggedInClick = function() {
-//     $('.not_logged_in').click(function(e) {
-//       e.preventDefault();
-//       $( "#login-form" ).dialog("open");
-//     });
-//   };
-//   initLoginDialog();
-//   bindNotLoggedInClick();
-// });
-
 (function(){
   var init = {
     initRegistrationDialog: function() {
-      var name = $( "#name" );
-      var email = $( "#email" );
-      var password = $( "#password" );
-      var password_confirmation = $( "#password_confirmation" );
+      var name = $( "#reg-name" );
+      var email = $( "#reg-email" );
+      var password = $( "#reg-password" );
+      var password_confirmation = $( "#reg-password_confirmation" );
       var allFields = $( [] ).add( name ).add( email ).add( password ).add( password_confirmation );
       var registrationHandler = function() {
         var bValid = true;
@@ -82,23 +34,23 @@
       });
     },
 
-    initLoginDialog: function() {
+    initLoginDialog: function() {      
       var email = $( "#email" );
       var password = $( "#password" );
       var allFields = $( [] ).add( email ).add( password );
       var loginHandler = function() {
         var bValid = true;
-        allFields.removeClass( "ui-state-error" );
+        $('#login-form input').removeClass( "ui-state-error" );
 
         bValid = bValid && Utilities.checkLength( email, "email", 6, 80 );
         bValid = bValid && Utilities.checkLength( password, "password", 6, 16 );
 
         if ( bValid ) {
-          var params = { user: { email: email.val(), password: password.val() } };
+          var params = { session: { email: email.val(), password: password.val() } };
           var callback = function() {
             window.location = '/';
           }
-          $.post('/login', params, callback);
+          $.post('/sessions', params, callback);
         }
       };
       $( "#login-form" ).dialog({
--- app/assets/javascripts/utilities.js
+++ app/assets/javascripts/utilities.js
@@ -3,14 +3,14 @@ var Utilities = {
     $( ".validateTips" )    
     .text( t )
     .addClass( "ui-state-highlight" );
-  // setTimeout(function() {
-  //   tips.removeClass( "ui-state-highlight", 1500 );
-  // }, 500 );    
+  setTimeout(function() {
+    tips.removeClass( "ui-state-highlight", 1500 );
+  }, 500 );    
   },
   checkLength: function( o, n, min, max ) {
     if ( o.val().length > max || o.val().length < min ) {
       o.addClass( "ui-state-error" );
-      updateTips( "Length of " + n + " must be between " +
+      Utilities.updateTips( "Length of " + n + " must be between " +
         min + " and " + max + "." );
       return false;
     } else {
